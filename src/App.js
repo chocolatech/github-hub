e27c208d7c9b5514672ccf8742dcc9084230ede4
cobbie <evelinagonda@gmail.com>
2017-12-04 15:42:45 +0100

    add margin to user list

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="form-group row">
-        <label htmlFor="input" className="col-1 col-form-label">Search: </label>
-        <div className="col-3">
-          <input type="text" className="form-control" />
+      <div>
+        <div className="form-group row">
+          <label htmlFor="input" className="col-1 col-form-label">Search: </label>
+          <div className="col-3">
+            <input type="text" className="form-control" />
+          </div>
+          <button type="button" className="btn btn-primary">Search</button>
         </div>
-        <button type="button" className="btn btn-primary">Search</button>
-        <div>
-
+        <div className="user-list">
+          <h3>Username</h3>
+          <ul className="list-group col-6">
+            <li className="list-group-item">First repo</li>
+            <li className="list-group-item">Second repo</li>
+          </ul>
         </div>
       </div>
     );
