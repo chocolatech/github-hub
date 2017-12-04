9ee4037bb4d1f7d406c844ab89d572a89d94afa0
cobbie <evelinagonda@gmail.com>
2017-12-04 15:00:46 +0100

    add bootstrap && search template

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <form>
+          <label htmlFor="input">Search: </label>
+          <input type="text"/>
+          <button type="button" className="btn btn-primary">Search</button>
+        </form>
+        <div>
 
+        </div>
       </div>
     );
   }
