17b8512fbaad490ed201b4d94a18ba34f4a4e3c6
cobbie <evelinagonda@gmail.com>
2017-12-04 15:13:46 +0100

    style search view

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <label htmlFor="input">Search: </label>
-          <input type="text"/>
-          <button type="button" className="btn btn-primary">Search</button>
-        </form>
+      <div className="form-group row">
+        <label htmlFor="input" className="col-1 col-form-label">Search: </label>
+        <div className="col-3">
+          <input type="text" className="form-control" />
+        </div>
+        <button type="button" className="btn btn-primary">Search</button>
         <div>
 
         </div>
